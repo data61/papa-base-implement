3b2e9fae38394bb7b0ebbf2b01cca5ab41313aa1
Tony Morris <tmorris@tmorris.net>
2017-01-07 18:59:12 +1000

    Control.Monad

--- src/Papa/Base/Implement.hs
+++ src/Papa/Base/Implement.hs
@@ -5,6 +5,7 @@ module Papa.Base.Implement(
 ) where
 
 import Papa.Base.Implement.Control.Applicative as P
+import Papa.Base.Implement.Control.Monad as P
 import Papa.Base.Implement.Data.Foldable as P
 import Papa.Base.Implement.Data.Function as P
 import Papa.Base.Implement.Data.List as P
