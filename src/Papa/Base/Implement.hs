aad8289822e2e39b8c35f8c62641229da1ae8e54
Tony Morris <tmorris@tmorris.net>
2017-01-07 19:32:51 +1000

    Data.Bool

--- src/Papa/Base/Implement.hs
+++ src/Papa/Base/Implement.hs
@@ -6,6 +6,7 @@ module Papa.Base.Implement(
 
 import Papa.Base.Implement.Control.Applicative as P
 import Papa.Base.Implement.Control.Monad as P
+import Papa.Base.Implement.Data.Bool as P
 import Papa.Base.Implement.Data.Foldable as P
 import Papa.Base.Implement.Data.Function as P
 import Papa.Base.Implement.Data.List as P
